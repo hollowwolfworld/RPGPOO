b35426c577410e1cbbff740cb0f111e7df22949a
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-09-02 14:40:33 +0200

    Ajout d'une super classe item et d'une class inventaire

---
+++ RPGPOO/Inventaire.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPOO
+{
+    class Inventaire
+    {
+        public int Taille {  get; private set; }
+        private Dictionary<Item, int> Item { get; set; }
+
+        public Inventaire(int taille, Dictionary<Item, int> item)
+        {
+            Taille = taille;
+            Item = item;
+        }
+
+        public Inventaire(int taille)
+        {
+            Taille = taille;
+            Item = new Dictionary<Item, int>();
+        }
+
+        public void AddItem(Item item, int quantity)
+        {
+
+        }
+
+        public void RemoveItem(Item item, int quantity)
+        {
+
+        }
+
+        public Item GetItem(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RPGPOO/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPOO
+{
+    abstract class Item
+    {
+        public string Nom { get; set; }
+        public string Description { get; set; }
+
+        protected Item(string nom, string description)
+        {
+            Nom = nom;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return Nom;
+        }
+    }
+}
