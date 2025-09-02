e84a5e622a93ac3e0bd1898c37a68ee77aea3f86
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-09-02 16:10:20 +0200

    Changement du nom de la classe guerrier de Guerrier vers Warrior

---
+++ RPGPOO/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPOO
+{
+    internal class Warrior : Player
+    {
+        protected Warrior(int XP, int pv, int attaque, int defence, int levels, string nom) : base(XP ,pv, attaque, defence, levels, nom)
+        {
+
+        }
+
+        public Warrior (string nom) : base(nom) 
+        {
+
+        }
+    }
+}
