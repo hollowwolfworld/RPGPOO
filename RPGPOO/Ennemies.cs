6bb7b3366c2c39c3a55ffa7394e4bab95d723609
Lou lc. Cuau <cda11cuau@2ISA.LAN>
2025-09-02 15:04:07 +0200

    j'ai cree une classe enemie et joueur

---
+++ RPGPOO/Ennemies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPOO
+{
+    internal class Ennemies : Entite
+    {
+        Ennemies(int pv, int attaque, int defence, int levels, string nom) : base(pv, attaque, defence, levels,nom)
+        {
+
+        }
+    }
+}
