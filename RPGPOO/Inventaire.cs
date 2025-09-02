0022f0032b4b8b09a1f94e94fd0f803dfcefb738
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-09-02 14:59:33 +0200

    Ajout de commentaire dans la classe Inventaire ainsi que changement de la methode GetItem en SearchItem.

--- RPGPOO/Inventaire.cs
+++ RPGPOO/Inventaire.cs
@@ -8,34 +8,73 @@ namespace RPGPOO
 {
     class Inventaire
     {
+        /// <summary>
+        /// Nombre maximum d'item que le joueur peut mettre dans son inventaire
+        /// </summary>
         public int Taille {  get; private set; }
-        private Dictionary<Item, int> Item { get; set; }
+        /// <summary>
+        /// Une collection d'Item avec leur quantité
+        /// </summary>
+        private Dictionary<Item, int> Items { get; set; }
 
-        public Inventaire(int taille, Dictionary<Item, int> item)
+        /// <summary>
+        /// Crée un inventaire avec un dictionnaire contenant déjà des items ainsi qu'une taille
+        /// </summary>
+        /// <param name="taille">La taille de l'inventaire</param>
+        /// <param name="item">Les items présent par </param>
+        public Inventaire(int taille, Dictionary<Item, int> items)
         {
             Taille = taille;
-            Item = item;
+            Items = items;
         }
 
+        /// <summary>
+        /// Crée un inventaire vide avec une taille
+        /// </summary>
+        /// <param name="taille"></param>
         public Inventaire(int taille)
         {
             Taille = taille;
-            Item = new Dictionary<Item, int>();
+            Items = new Dictionary<Item, int>();
         }
 
+        /// <summary>
+        /// Ajoute un objet ainsi que la quantité à ajouter
+        /// </summary>
+        /// <param name="item">L'objet à ajouter à l'inventaire</param>
+        /// <param name="quantity">La quantité de l'objet à ajouter</param>
         public void AddItem(Item item, int quantity)
         {
 
         }
 
+        /// <summary>
+        /// Enlève une certaine quantité d'un objet à l'inventaire
+        /// </summary>
+        /// <param name="item">L'objet à enlever</param>
+        /// <param name="quantity">La quantité à enlever</param>
         public void RemoveItem(Item item, int quantity)
         {
 
         }
 
-        public Item GetItem(string name)
+        /// <summary>
+        /// Recherche l'objet et sa quantité avec son nom
+        /// </summary>
+        /// <param name="name">Le nom de l'objet</param>
+        /// <returns>La liste des objets trouver contenant ce nom avec leur quantité dans l'inventaire</returns>
+        public Dictionary<Item, int> SearchItem(string name)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Récupère l'inventaire dans son entierter
+        /// </summary>
+        /// <returns>Les objets avec leur quantités</returns>
+        public Dictionary<Item, int> GetInventory()
+        {
+            return Items;
         }
     }
 }
