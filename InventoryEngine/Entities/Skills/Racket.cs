5fd30386ede63a41ff440ef7d6e823cba71ddeb3
hollowwolfworld <l.cuau23@gmail.com>
2025-09-03 18:54:30 +0200

    creation de la class racket

---
+++ InventoryEngine/Entities/Skills/Racket.cs
@@ -0,0 +1,21 @@
+﻿using EntityEngine.Entities.Enemies;
+using EntityEngine.Exceptions;
+using FightEngine.Skills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityEngine.Entities.Skills
+{
+    public class Racket : ISkill
+    {
+        public void UseSkill(IEntity from, IEntity to)
+        {
+            if (from is not Thugs) throw new NotAllowedToUseSkill();
+
+            DamageCalculator.CalculateDamage(from.) //to do scale sur l'argent
+        }
+    }
+}
