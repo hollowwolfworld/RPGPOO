3b922a911b3bfef45aa78ac5568271809d273086
LouisDufourd <102533233+LouisDufourd@users.noreply.github.com>
2025-09-04 16:17:20 +0200

    ajout de builder pour la classe Slime et Wizzard

--- InventoryEngine/Entities/Enemies/Wizzard.cs
+++ InventoryEngine/Entities/Enemies/Wizzard.cs
@@ -1,4 +1,5 @@
-﻿using EntityEngine.Inventories;
+﻿using EntityEngine.Entities.Players;
+using EntityEngine.Inventories;
 using FightEngine.Skills;
 using System;
 using System.Collections.Generic;
@@ -24,9 +25,10 @@ namespace EntityEngine.Entities.Enemies
         private int luck;
         private int gold;
         private Inventory inventory;
-        
-        public Wizzard(int maxHp, int hp, int maxMp, int attack, int defence, int speed, int luck, int mp, int levels, int gold, string name)
+
+        public Wizzard(string name, int maxHp, int hp, int maxMp, int mp, int attack, int defence, int speed, int levels, int luck, int gold, List<ISkill> skills, Dictionary<Status, int> status, Inventory inventory)
         {
+            this.name = name;
             this.maxHp = maxHp;
             this.hp = hp;
             this.maxMp = maxMp;
@@ -35,12 +37,11 @@ namespace EntityEngine.Entities.Enemies
             this.defence = defence;
             this.speed = speed;
             this.levels = levels;
-            this.name = name;
             this.luck = luck;
-            this.name = name;
-            this.skills = new List<ISkill>();
-            this.status = new Dictionary<Status, int>();
-            this.inventory = new Inventory(5);
+            this.gold = gold;
+            this.skills = skills;
+            this.status = status;
+            this.inventory = inventory;
         }
 
         public int MaxHealthPoint { get => maxHp; set => maxHp = value; }
@@ -90,6 +91,101 @@ namespace EntityEngine.Entities.Enemies
         public Move ChooseMove()
         {
             throw new NotImplementedException();
+        }
+
+        public class Builder
+        {
+            private string name = "Wizzard";
+            private int level = 1;
+            private int maxHealth = 22;
+            private int health = 22;
+            private int maxMp = 10;
+            private int mp = 10;
+            private int attack = 4;
+            private int defence = 3;
+            private int speed = 5;
+            private int luck = 3;
+            private int gold = 0;
+            private Dictionary<Status, int> status = new Dictionary<Status, int>();
+            private List<ISkill> skills = LevelManager.GetSkillsForLevel(1, typeof(Wizzard));
+            private Inventory inventory = new Inventory(5);
+
+            public Wizzard Build()
+            {
+                return new Wizzard(name, level, maxHealth, health, maxMp, mp, attack, defence, speed, luck, gold, skills, status, inventory);
+            }
+
+            public Builder SetName(string name)
+            {
+                this.name = name;
+                return this;
+            }
+
+            public Builder SetLevel(int level)
+            {
+                this.level = level;
+                return this;
+            }
+
+            public Builder SetMaxHealth(int maxHealth)
+            {
+                this.maxHealth = maxHealth;
+                return this;
+            }
+
+            public Builder SetHealth(int health)
+            {
+                this.health = health;
+                return this;
+            }
+
+            public Builder SetAttack(int attack)
+            {
+                this.attack = attack;
+                return this;
+            }
+
+            public Builder SetDefence(int defence)
+            {
+                this.defence = defence;
+                return this;
+            }
+
+            public Builder SetSpeed(int speed)
+            {
+                this.speed = speed;
+                return this;
+            }
+
+            public Builder SetLuck(int luck)
+            {
+                this.luck = luck;
+                return this;
+            }
+
+            public Builder SetGold(int gold)
+            {
+                this.gold = gold;
+                return this;
+            }
+
+            public Builder SetStatus(Dictionary<Status, int> status)
+            {
+                this.status = status;
+                return this;
+            }
+
+            public Builder SetSkills(List<ISkill> skills)
+            {
+                this.skills = skills;
+                return this;
+            }
+
+            public Builder SetInventory(Inventory inventory)
+            {
+                this.inventory = inventory;
+                return this;
+            }
         }
     }
 }
